a5b41f1fd792eb659f8aef9dd242431406311bef
HarshithaBoppudi <67325628+HarshithaBoppudi@users.noreply.github.com>
2021-07-02 20:30:42 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig={
+    apiKey: "AIzaSyB1DN6MH64LEbdGU4RUSJkfRKu9r01Ty6o",
+    authDomain: "storytellingapp-af708.firebaseapp.com",
+    projectId: "storytellingapp-af708",
+    storageBucket: "storytellingapp-af708.appspot.com",
+    messagingSenderId: "728627569095",
+    appId: "1:728627569095:web:53b62c3ea441bd6c4c79e2"
+}
